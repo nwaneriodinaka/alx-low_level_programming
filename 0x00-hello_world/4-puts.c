a83a2e5cc064ffbc05218121af75d9838f81b382
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-07 12:35:18 -0700

    hello world

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -11,7 +11,7 @@ int main(void)
 {
 	char str1[52];
 
-	strcpy(str1, "\"programming is like building a multilingual puzzle");
+	strcpy(str1, "\"Programming is like building a multilingual puzzle");
 	puts(str1);
 
 	return (0);
