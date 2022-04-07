d43d653e7c3cd4d2ad549e43034bbc875140f96f
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-07 12:58:55 -0700

    hello world

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -8,7 +8,7 @@
  *
  *  
  * * Return: Always 0 (success)
- * */
+ */
 
 int main(void)
 
@@ -19,6 +19,7 @@ int main(void)
 	long int e;
 	long long int f;
 	float g;
+
 	printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
 	printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(i));
 	printf("Size of a long int: %lu byte(s)\n", (unsigned long)sizeof(e));
