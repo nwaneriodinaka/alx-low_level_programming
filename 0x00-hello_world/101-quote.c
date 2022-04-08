e7dbfcecb107ce15a774ee832ba43b697e399715
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-07 21:13:19 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,1 +1,13 @@
-#include
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - Entry point
+ *
+ * Return
+ */
+int main(void)
+{
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 47);
+	return (0);
+}
