ccbfa8427ea83bd14db3bb2396f5273ed35e0158
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-08 11:50:34 -0700

    c variables

--- 0x01-variables_if_else_while/0-positive_or_negative.c
+++ 0x01-variables_if_else_while/0-positive_or_negative.c
@@ -13,7 +13,7 @@
 int main(void)
 {
 	int n;
-	
+
 	srand(time(0));
 	n = rand() - RAND_MAX / 2;
 	if (n > 0)
@@ -29,4 +29,4 @@ int main(void)
 		printf("%d is negative\n", n);
 	}
 		return (0);
-}	
+}
