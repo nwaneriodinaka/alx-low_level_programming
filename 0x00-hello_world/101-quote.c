468a0a0c9ced0472f5a803ab550db4530899d124
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-07 21:18:05 -0700

    hello world

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -4,10 +4,10 @@
 /**
  * main - Entry point
  *
- * Return
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
-	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 47);
+	write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19", 59);
 	return (0);
 }
