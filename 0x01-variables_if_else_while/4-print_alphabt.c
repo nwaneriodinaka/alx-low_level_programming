c338c0fd9bfce893b26705dc1568bcae1c8813df
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-08 21:34:29 -0700

    c variables

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'A'; x <= 'Z'; ++x)
+	{
+		if (x != 'Q' && x != 'E')
+		{
+			putchar(tolower(x));
+		}
+	}
+
+		putchar('\n');
+
+		return (0);
+}
