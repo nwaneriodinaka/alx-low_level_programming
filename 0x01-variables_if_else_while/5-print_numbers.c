36f42fea2b83f911439deb970b4c7c6f174cb3ca
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-09 15:01:02 -0700

    fixing errors

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -10,52 +10,8 @@ int main(void)
 {
 	int x;
 
-	for (x = 0; x < 10; ++x)
+	for (x = '0'; x <= '9'; ++x)
 	{
-		printf("%d", x);
+		printf("%d, x" \n);
 	}
-
-	printf("\n");
-
-	return (0)
-}#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int x;
-
-	for (x = 0; x < 10; ++x)
-	{
-		printf("%d", x);
-	}
-
-	printf("\n");
-
-	return (0)
-}#include <stdio.h>
-
-/**
- * main - Entry point
- *
- * Return: Always 0 (Success)
- */
-
-int main(void)
-{
-	int x;
-
-	for (x = 0; x < 10; ++x)
-	{
-		printf("%d", x);
-	}
-
-	printf("\n");
-
-	return (0)
 }
