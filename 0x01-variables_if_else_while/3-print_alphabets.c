ff4cc02470ea7786aa2f516de570578c46eb2c5f
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-08 21:19:02 -0700

    c variables

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+ int main(void)
+{
+	int x, y;
+
+	for (x = 'A' ; x <= 'Z' ; ++x)
+	{
+		putchar(tolower(x));
+	}
+	for (y = 'A' ; y <= 'Z'; ++y)
+	{
+		putchar(y);
+	}
+	putchar('\n');
+
+	return (0);
+}
