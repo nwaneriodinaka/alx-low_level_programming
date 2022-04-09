4d17157659653449f0eb279185193594bf563437
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-08 21:09:09 -0700

    c variables

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,13 +4,13 @@
 /**
  * main - Entry point
  *
- * Returm: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	int x, lower_x;
 
-	for (x = 'A'; x<= 'Z'; x++);
+	for (x = 'A'; x <= 'Z'; x++);
 	{
 		lower_x = tolower(x);
 		putchar(lower_x);
