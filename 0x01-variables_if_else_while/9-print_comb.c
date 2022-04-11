00d292e54aaed85956a14181295ebc778acbdf04
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-11 10:32:42 -0700

    Fixed bugs

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -3,19 +3,22 @@
 /**
  * main - Entry
  *
- * Remain: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 
 int main(void)
 {
 	int x;
+	int y = 0;
 
-	for (x = '0'; x <= '9'; x++)
+	for (x = '0'; x <= '9'; x++, y++)
 	{
-
 		putchar(x);
+	if (y < 9)
+		{
 	putchar(',');
 	putchar(' ');
+	}
 	}
 	putchar('\n');
 
