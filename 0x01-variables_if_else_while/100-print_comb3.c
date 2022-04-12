6b8364a88e4e63476bae02e90e199535419778af
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-11 21:31:09 -0700

    print_comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ * main - Entry
+ *
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int x, y;
+
+	for (x = '0'; x < '9'; x++)
+	{
+		for (y = x + 1; y <= '9'; y++)
+		{
+			if (x != y)
+			{
+				putchar(x);
+				putchar(y);
+		if (x == '8' && y == '9')
+		continue;
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
