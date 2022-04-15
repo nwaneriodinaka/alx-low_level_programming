b3418b866ac67f287446a291623d72163a8fd969
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-15 15:23:09 -0700

    debugging task

--- 0x03-debugging/2-largest_number.c
+++ 0x03-debugging/2-largest_number.c
@@ -14,11 +14,11 @@ int largest_number(int a, int b, int c)
 
 	if (a >= b && a >= c)
 	{
-		largest = a
+		largest = a;
 	}
 	else if (b >= a &&b >= c)
 	{
-		largest = b
+		largest = b;
 	}
 	else
 	{
