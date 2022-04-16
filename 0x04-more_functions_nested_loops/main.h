06ff18383e4324253909bc9d5370d136b9fc34a0
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-16 00:18:30 +0000

    advanced nestloop task

---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,25 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/*
+ * File: main.h
+ * 
+ * Description: Header file containing declarations for 
+ * all functions used in the 0x02-functions_nested_loops 
+ * directory.
+ */
+
+int _putchar(char c);
+void print_numbers(void);
+void print_most_numbers(void);
+int _isupper(int c);
+int _isdigit(int c);
+void print_number(int n);
+void print_triangle(int size);
+void more_numbers(void);
+void print_line(int n);
+int mul(int a, int b);
+void print_diagonal(int n);
+void print_square(int n);
+
+#endif
