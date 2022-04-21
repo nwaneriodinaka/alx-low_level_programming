bb635a642dbf1b4444652f56254c60611d6057aa
nwaneriodinaka <nwaneriodinaka@gmail.com>
2022-04-21 19:32:23 +0000

    tasks on pointers and array

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _strlen - function that returns the lenght of a string
+ *
+ * @s: pointer to a string
+ * Return: int
+ */
+
+int _strlen(char *s);
+{
+	int i = 0;
+
+	while (s[i] != '\0')
+	{
+		i += 1;
+	}
+	return (i);
+}
